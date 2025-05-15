48ab32317ab4ffb2ba92ccccffa4b379559728f4
nevil <nevil.oporo@student.moringaschool.com>
2025-05-15 17:25:05 +0300

    object

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,12 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, color, wheel_number=4):
+        self.wheel_size = wheel_size
+        self.color = color
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self):
+        return "filling up!"
+
